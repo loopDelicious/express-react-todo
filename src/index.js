bc8c618ad26e4cfd772a52a227b9e134ba2c82c6
Joyce Lin <joycelin79@gmail.com>
2016-10-22 00:00:43 -0700

    add reset and buttons for css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './javascripts/App';
+import './css/reset.css';
 import './css/variables.css';
 import './css/index.css';
 import './css/navigation.css';
+import './css/buttons.css';
 import './css/login.css';
 import './css/App.css';
 
